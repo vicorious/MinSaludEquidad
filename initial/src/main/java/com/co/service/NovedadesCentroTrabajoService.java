fb50bf4d5661be173105397fe8c864380e383354
Alejandro Lindarte Castro <alejo.lindarte@outlook.com>
2020-08-04 15:40:47 -0500

    Beans x2

--- initial/src/main/java/com/co/service/NovedadesCentroTrabajoService.java
+++ initial/src/main/java/com/co/service/NovedadesCentroTrabajoService.java
@@ -6,11 +6,15 @@ import com.co.exception.MinSaludBusinessException;
 import com.co.persistence.NovedadCentroTrabajoRepository;
 import com.co.persistence.NovedadesTransitoriasRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
 
 import java.math.BigDecimal;
 import java.util.Arrays;
 import java.util.List;
 
+@Component
+@Service
 public class NovedadesCentroTrabajoService
 {
     @Autowired
