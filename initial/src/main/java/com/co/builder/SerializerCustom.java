1e94a41932991f71ba4534812bd890643b45af2b
Alejandro Lindarte Castro <alejo.lindarte@outlook.com>
2020-09-02 13:20:28 -0500

    Update charset for request

--- initial/src/main/java/com/co/builder/SerializerCustom.java
+++ initial/src/main/java/com/co/builder/SerializerCustom.java
@@ -6,6 +6,7 @@ import com.fasterxml.jackson.databind.SerializerProvider;
 
 import java.io.IOException;
 import java.nio.charset.StandardCharsets;
+import java.text.Normalizer;
 
 public class SerializerCustom extends JsonSerializer<String>
 {
@@ -13,6 +14,13 @@ public class SerializerCustom extends JsonSerializer<String>
     public void serialize(String value, JsonGenerator gen, SerializerProvider serializers) throws IOException
     {
         String val = value.trim();
-        gen.writeString(new String(val.getBytes(), StandardCharsets.ISO_8859_1));
+        gen.writeString(eliminaAcentos(val));
+    }
+
+    public String eliminaAcentos(String s) {
+        if(s == null || s.trim().length() == 0) {
+            return "";
+        }
+        return Normalizer.normalize(s, Normalizer.Form.NFD).replaceAll("[\u0300-\u0301]", "");
     }
 }
